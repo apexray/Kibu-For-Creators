8726b04643507c46547ff41b603ae0bee0b3861f
rishabh goswami <rishabhgoswami7011@gmail.com>
2024-03-11 12:47:23 +0530

    added modules

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const hbs = require("express-handlebars");
+
+app.use(express.json());
+
+//setup static file
+app.use(express.static(path.join(__dirname, "public")));
+
+//setup views engine
+app.set("view engine", "hbs");
+app.engine(
+  "hbs",
+  hbs({
+    extname: "hbs",
+    defaultView: "default",
+    layoutsDir: path.join(__dirname, "views"),
+    partialsDir: path.join(__dirname, "views/partials"),
+  })
+);
+
+//routes
+app.get("/", (req, res) => {
+  res.send("<h1>express server is started</h1>");
+});
+
+app.listen(3000, () =>
+  console.log(`server is lisend on port http://localhost:3000`)
+);
